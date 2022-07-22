f426d1d5953cf15f0b0e1543dbd9bddda53fc025
Marko Krezic <68282076+kreza-dzd@users.noreply.github.com>
2022-07-22 16:49:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const navSlide = () => {
+   const menuIcon = document.querySelector('#menu-icon');
+   const nav = document.querySelector('.navbar');
+   const navbar = document.querySelectorAll('.navbar li');
+  
+
+ 
+
+   menuIcon.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      navbar.forEach((link, index) => {
+
+         if (link.style.animation) {
+            link.style.animation = '';
+         } else {
+            link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 0.3}s`;
+         }
+      });
+   });
+
+
+
+}
+
+navSlide();
+
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+    if (link.href.includes(`${karriere}`)) {
+     link.classList.add('active');
+    }
+})
